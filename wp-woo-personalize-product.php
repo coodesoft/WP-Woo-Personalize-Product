9f806036a01f6f5e28a2c7aca6a7f8da9fd572ac
usuario <lucho.2012.tandil@gmail.com>
2019-05-06 13:35:33 -0300

    Cración de estructura de directorios básica

---
+++ wp-woo-personalize-product.php
@@ -0,0 +1,13 @@
+/*
+Plugin Name: WP-Woo-Personalize-Product
+Plugin URI:
+Description: Plugin de creación de productos personalizados, para ser usado en conjunto con WooCommerce, por parte de posibles clientes.
+Version: 1.0
+Author: Coodesoft
+Author URI: https://www.coodesoft.com.ar
+License: GPL v3
+*/
+
+<?php
+
+?>
